a7b9a4f6d4e97c2c89c4392036ced69b54d3b3d6
Noah Dujovny <dujovny@umich.edu>
2023-05-06 11:39:28 -0400

    added navbar and pageviewer components and implemented them into app.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav 
+    :class= "[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']"
+    >
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <NavBarLink
+                        :page="page"
+                        :isActive="activePage === index"
+                        @click.prevent="navLinkClick(index) "
+                    ></NavBarLink>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button 
+                    class="btn btn-primary" 
+                    @click.prevent="changeTheme()"
+                >Toggle</button>
+            </form>
+        </div>
+    </nav> 
+</template>
+
+<script>
+import NavBarLink from './NavBarLink.vue';
+
+export default {
+    components:{
+        NavBarLink
+    },
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data() {
+        return {
+            theme: 'dark'
+        };
+    },
+    methods: {
+        changeTheme() {
+            let theme = 'light';
+
+            if (this.theme == 'light') {
+                theme = 'dark';
+            }
+            this.theme = theme;
+        }
+    }
+}
+</script>
